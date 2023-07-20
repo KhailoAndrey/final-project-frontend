9f28920a38b6ddae747a45d378bef58aaa9ede44
KhailoAndrey <hap512@gmail.com>
2023-07-21 01:28:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { store, persistor } from './redux/store';
 import i18n from './i18n';
-import { store, persistor } from './redux/store';
 import { App } from 'components/App';
 
 import './index.css';
