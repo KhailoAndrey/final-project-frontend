0d96bfc26ecf22df3ec549e4255e0e94decf012f
Yriy Ogorodnik <yriyelektric@gmail.com>
2023-07-22 00:27:45 +0300

    conteiner section в llayout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,16 +2,19 @@ import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import Loader from 'components/Loader/Loader';
 import Header from 'components/Header/Header';
-import { Container } from './Layout.styled';
+import { Container, Section } from './Layout.styled';
 
 const Layout = () => {
   return (
     <>
       <Suspense fallback={<Loader />}>
+        <Section>
         <Container>
           <Header />
           <Outlet />
         </Container>
+        </Section>
+       
       </Suspense>
     </>
   );
--- src/pages/MainPage/MainPage.styled.js
+++ src/pages/MainPage/MainPage.styled.js
@@ -1,64 +1,4 @@
 import styled from 'styled-components';
-import * as bg from '../../images/BackGround/index';
-
-export const Section = styled.section`
-  max-width: 100%;
-  background-color: #fdf7f2;
-
-  max-height: calc(100vh - 48px);
-  margin: 0 auto;
-  background-repeat: no-repeat;
-  background-position: center top;
-  background-size: 100%;
-  background-image: url(${bg.bg_mob});
-
-  @media screen and (min-device-pixel-ratio: 2),
-    (min-resolution: 192dpi),
-    (min-resolution: 2dppx) {
-    background-image: url(${bg.bg_mob_2x});
-  }
-
-  @media screen and (min-width: 768px) {
-    height: calc(100vh - 68px);
-    background-image: url(${bg.bg_tab});
-
-    @media screen and (min-device-pixel-ratio: 2),
-      (min-resolution: 192dpi),
-      (min-resolution: 2dppx) {
-      background-image: url(${bg.bg_tab_2x});
-    }
-  }
-
-  @media screen and (min-width: 1280px) {
-    max-height: calc(100vh - 72px);
-    background-image: url(${bg.bg_desk});
-
-    @media screen and (min-device-pixel-ratio: 2),
-      (min-resolution: 192dpi),
-      (min-resolution: 2dppx) {
-      background-image: url(${bg.bg_desk});
-    }
-  }
-`;
-
-export const Container = styled.div`
-  margin: 0 auto;
-  width: 320px;
-  padding-left: 20px;
-  padding-right: 20px;
-
-  @media screen and (min-width: 768px) {
-    width: 786px;
-    padding-left: 32px;
-    padding-right: 32px;
-  }
-
-  @media screen and (min-width: 1280px) {
-    width: 1280px;
-    padding-left: 16px;
-    padding-right: 16px;
-  }
-`;
 
 export const Wrapper = styled.div`
   margin: 0 auto;
@@ -69,7 +9,7 @@ export const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  gap: 20px;
+  /* gap: 20px; */
 
   @media screen and (min-width: 768px) {
     padding-top: 80px;
@@ -88,55 +28,48 @@ export const Title = styled.h1`
   width: 100%;
   font-weight: 700;
   font-size: 32px;
-  line-height: calc(44 / 32);
+  line-height: 1.375;
   margin: 0;
   @media screen and (min-width: 768px) {
     font-size: 68px;
-    line-height: calc(100 / 68);
+    line-height: 1.47;
   }
 
   @media screen and (min-width: 1280px) {
     margin-top: 188px;
     max-width: 501px;
-    min-width: 501px;
     font-weight: 800;
     font-size: 68px;
-    line-height: calc(88 / 68);
+    line-height: 1.29;
   }
 `;
 
 export const Picture = styled.picture`
   display: flex;
   justify-content: center;
-  width: 100vw;
-  overflow: hidden;
-  height: calc(100vh - 216px);
+  width: 100%;
   min-height: 394px;
-
   @media screen and (min-width: 768px) {
-    min-width: 100vw;
+    width: 985px;
     height: 818px;
   }
 
   @media screen and (min-width: 1280px) {
-    margin-top: -50px;
+    width: 917px;
+    height: 670px;
     margin-left: -154px;
-    min-width: auto;
-    width: calc(100vw - 363px);
-    height: calc(100vh - 52px);
   }
 `;
 
 export const Img = styled.img`
-  min-width: 475px;
+  min-width: 474px;
   max-height: 494px;
   object-fit: cover;
   object-position: center;
 
   @media screen and (min-width: 768px) {
-    margin-top: -100px;
-    min-width: 960px;
-    min-height: 915px;
+    min-width: 985px;
+    min-height: 818px;
   }
 
   @media screen and (min-width: 1280px) {
