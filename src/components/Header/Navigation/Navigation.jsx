ac3f0e7cf5ca99a6e1b8409bcd60251818c97549
KhailoAndrey <hap512@gmail.com>
2023-07-21 20:52:39 +0300

    add logo & nav

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,5 +1,21 @@
+// import { useAuth } from 'redux/auth/selectors';
+import AuthNav from './AuthNav/AuthNav';
+import Nav from './Nav/Nav';
+import UserNav from './UserNav/UserNav';
+import ModalBurger from 'components/Modals/ModalBurger/ModalBurger';
+
+// const { isLoggedIn } = useAuth();
+
+const isLoggedIn = true;
+
 const Navigation = () => {
-  return <></>;
+  return (
+    <>
+      <Nav />
+      {isLoggedIn ? <UserNav /> : <AuthNav />}
+      <ModalBurger />
+    </>
+  );
 };
 
 export default Navigation;
