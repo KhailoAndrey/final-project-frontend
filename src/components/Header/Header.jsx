45ad9334048d9925960a8448e072255e279a66d3
KhailoAndrey <hap512@gmail.com>
2023-07-21 16:04:30 +0300

    fix routes, add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,14 @@
+import Logo from './Logo/Logo';
+import Navigation from './Navigation/Navigation';
+import { Container } from './Header.styled';
+
 const Header = () => {
-  return <>Header</>;
+  return (
+    <Container>
+      <Logo />
+      <Navigation />
+    </Container>
+  );
 };
 
 export default Header;
