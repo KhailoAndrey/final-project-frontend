be1bbb84f2d3c7441168024405e283d885409256
Yriy Ogorodnik <yriyelektric@gmail.com>
2023-07-22 21:28:46 +0300

    d

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,13 @@
-<<<<<<< HEAD
 
-=======
 import { Wrapper, Title, Picture, Img } from './MainPage.styled';
 import * as mainImg from '../../images/Home';
->>>>>>> main
+import ModalCongrats from 'components/Modals/ModalCongrats/ModalCongrats';
+
 
 const MainPage = () => {
   return (
     <>
+    <ModalCongrats></ModalCongrats>
       <Wrapper>
         <Title>Take good care of your small pets</Title>
         <Picture>
