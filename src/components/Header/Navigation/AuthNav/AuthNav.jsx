2b8a3d4b4bbf1975db8b94444bde4dbf9b5f30e3
KhailoAndrey <hap512@gmail.com>
2023-07-23 10:45:41 +0300

    add logout btn

--- src/components/Header/Navigation/AuthNav/AuthNav.jsx
+++ src/components/Header/Navigation/AuthNav/AuthNav.jsx
@@ -1,5 +1,16 @@
+import { AuthBox } from './AuthNav.styled';
+import { LoginBtn } from './Login/loginBtn';
+import { RegistrationBtn } from './Registration/registrBtn';
+
 const AuthNav = () => {
-  return <></>;
+  return (
+    <>
+      <AuthBox>
+        <LoginBtn></LoginBtn>
+        <RegistrationBtn></RegistrationBtn>
+      </AuthBox>
+    </>
+  );
 };
 
 export default AuthNav;
--- src/components/Header/Navigation/AuthNav/Login/loginBth.styled.js
+++ src/components/Header/Navigation/AuthNav/Login/loginBth.styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ButtonLog = styled.button`
   border-radius: 40px;
@@ -6,15 +6,27 @@ export const ButtonLog = styled.button`
   background: var(--main-clr-yellow);
   display: flex;
   width: 165px;
+  height: 40px;
   padding: 8px 20px;
   justify-content: center;
   align-items: center;
   gap: 8px;
-
-  color: #fef9f9;
+  color: var(--btn-clr-fon);
   font-family: Manrope;
   font-size: 16px;
   font-weight: 700;
+  transition: all var(--default-animation);
+  svg {
+    fill: var(--btn-clr-fon);
+  }
+  &:hover {
+    background: var(--main-bckg-clr);
+    color: var(--main-clr-yellow);
+    svg {
+      fill: var(--main-clr-yellow);
+    }
+    transform: transition;
+  }
 
   @media screen and (min-width: 768px) {
     font-size: 20px;
