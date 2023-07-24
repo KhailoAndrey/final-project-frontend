b7909d06379075343ec113cbf9d3a8f3817799da
KhailoAndrey <hap512@gmail.com>
2023-07-24 13:09:27 +0300

    fix login form & route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -34,7 +34,7 @@ export const App = () => {
             path="login"
             element={
               <PublicRoute
-                redirectTo="/main"
+                redirectTo="/user"
                 component={<LoginPage />}
               ></PublicRoute>
             }
