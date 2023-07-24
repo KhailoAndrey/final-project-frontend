5409cfcdfe8141b7809cae1e523e51770877165d
Nika <99656127+NikaLunina@users.noreply.github.com>
2023-07-24 18:07:11 +0200

    add hover&focus

--- src/components/Forms/LoginForm.jsx
+++ src/components/Forms/LoginForm.jsx
@@ -29,13 +29,13 @@ const emailRegexp = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
 const validationSchema = Yup.object({
   email: Yup.string()
     .email('Please enter your registered email')
-    .required('Please enter your email')
-    .matches(emailRegexp, 'Please enter a valid value using English characters'),
+    .required('Email is a required field')
+    .matches(emailRegexp, 'Enter a valid Email'),
   password: Yup.string()
     .trim()
-    .required('Please enter password')
-    .min(6, 'The password you entered is incorrect. Please try again')
-    .max(32, 'The password you entered is incorrect. Please try again'),
+    .required('Password is a required field')
+    .min(6, 'Password must be at least 6 characters')
+    .max(16, 'Password must be no more than 16 characters'),
 });
 
 export const LoginForm = () => {
