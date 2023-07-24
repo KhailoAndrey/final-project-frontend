4a7216def0a55ee7eda37febbff4342aed2ea484
Dmytro Potapenko <dima.lmkh@gmail.com>
2023-07-24 14:59:13 +0300

    fix btn

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,14 +1,21 @@
 import { NFText, NFImg, NFDivWrap, Button, PawIcon } from './NotFound.styled';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+// import svg from '../../../images/Icons/symbol-defs.svg';
 
 export const NotFound = () => {
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate('/');
+  };
   return (
     <>
       <NFDivWrap>
         <NFText>Oops! This page not found...</NFText>
         <NFImg />
-        <Button type="button">
-          <Link to="/">To main page</Link>
+        <Button type="button" onClick={() => onClick()}>
+          To main page
           <PawIcon />
         </Button>
       </NFDivWrap>
