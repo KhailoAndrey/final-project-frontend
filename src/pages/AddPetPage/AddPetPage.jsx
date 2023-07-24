ceb2c93fd4881ab78372ea66f7166bb84e7a2c82
kravets-julia <kravets_julia@i.ua>
2023-07-24 12:59:01 +0300

    AddPetPage

--- src/pages/AddPetPage/AddPetPage.jsx
+++ src/pages/AddPetPage/AddPetPage.jsx
@@ -1,16 +1,16 @@
 import { AddPetForm } from 'components/Forms/AddPetForm/AddPetForm';
-import { Section } from 'components/Forms/AddPetForm/Section/Section';
-import { Container } from 'components/Forms/AddPetForm/Container/Container';
+// import { Section } from 'components/Forms/AddPetForm/Section/Section';
+// import { Container } from 'components/Forms/AddPetForm/Container/Container';
 import { BgWrapper } from './Pages.styled';
 
 const AddPetPage = () => {
   return (
     <BgWrapper>
-      <Section padTopSm={'20px'} padTopMd={'36px'} padTopLg={'20px'}>
-        <Container>
-          <AddPetForm />
-        </Container>
-      </Section>
+      {/* <Section padTopSm={'20px'} padTopMd={'36px'} padTopLg={'20px'}>
+        <Container> */}
+      <AddPetForm />
+      {/* </Container>
+      </Section> */}
     </BgWrapper>
   );
 };
