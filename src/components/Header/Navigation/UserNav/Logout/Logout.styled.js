7b95bb28b2d4844cb055f64cc8c1b2379017db11
KhailoAndrey <hap512@gmail.com>
2023-07-26 02:36:26 +0300

    refactor button logout

--- src/components/Header/Navigation/UserNav/Logout/Logout.styled.js
+++ src/components/Header/Navigation/UserNav/Logout/Logout.styled.js
@@ -1,40 +1,43 @@
 import styled from 'styled-components';
 
 export const LogoutBtn = styled.button`
-      display: ${({ showButton }) => (showButton ? 'inline-flex' : 'none')};
-  margin-left: 0;
-  margin-right: auto;
-    margin-top: 160px;
-    border-radius: 40px;
-    border: 2px solid var(--main-clr-blue);
-    background: var(--main-clr-blue);
-    padding: 8px;
-    gap: 8px;
-    justify-content: center;
-    align-items: center;
-    width: 135px;
-    height: 40px;
-    font-family: 'Manrope';
-    font-size: 16px;
-    font-weight: 700;
-    color: var(--btn-clr-fon);
-    letter-spacing: 0.64px;
-    transition: all var(--default-animation);
+  display: ${({ showButton }) => (showButton ? 'inline-flex' : 'none')};
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  border-radius: 40px;
+  border: 2px solid var(--main-clr-blue);
+  background: var(--main-clr-blue);
+  padding: 8px;
+  gap: 8px;
+  justify-content: center;
+  align-items: center;
+  width: 135px;
+  height: 40px;
+  font-family: 'Manrope';
+  font-size: 16px;
+  font-weight: 700;
+  color: var(--btn-clr-fon);
+  letter-spacing: 0.64px;
+  transition: all var(--default-animation);
+  svg {
+    stroke: var(--btn-clr-fon);
+  }
+  &:hover {
+    background: var(--btn-clr-fon);
+    color: var(--main-clr-blue);
     svg {
-      stroke: var(--btn-clr-fon);
+      stroke: var(--main-clr-blue);
     }
-    &:hover {
-      background: var(--btn-clr-fon);
-      color: var(--main-clr-blue);
-      svg {
-        stroke: var(--main-clr-blue);
-      }
-      transform: transition;
-    }
-    @media screen and (min-width: 768px) {
-      /* display: ${({ showButton }) => (showButton ? 'inline-flex' : 'none')}; */
+    transform: transition;
+  }
+  @media screen and (min-width: 768px) {
+    left: auto;
+    right: 80px;
+    top: 26px;
   }
   @media screen and (min-width: 1280px) {
+    position: static;
     display: inline-flex;
     margin-left: auto;
     margin-top: 0;
