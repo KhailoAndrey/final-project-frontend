f37f2ae1914e64cb45c92ac409fe0a150c81b292
KhailoAndrey <hap512@gmail.com>
2023-07-30 02:19:58 +0300

    add image refactor

--- src/images/Home/index.js
+++ src/images/Home/index.js
@@ -6,3 +6,12 @@ export { default as home_tablet_2x } from './home_tab_2x.png';
 
 export { default as home_mobile_1x } from './home_mob.png';
 export { default as home_mobile_2x } from './home_mob_2x.png';
+
+export { default as home_desk } from './home_desk.webp';
+export { default as home_desk2 } from './home_desk_2x.webp';
+
+export { default as home_tab } from './home_tab.webp';
+export { default as home_tab2 } from './home_tab_2x.webp';
+
+export { default as home_mob } from './home_mob.webp';
+export { default as home_mob2 } from './home_mob_2x.webp';
