624f38442cdf7145553cc2424055b79ad7ee3611
KhailoAndrey <hap512@gmail.com>
2023-08-03 19:45:56 +0300

    Update ToTopBtn.styled.js

--- src/helpers/ScrollBtn/ToTopBtn.styled.js
+++ src/helpers/ScrollBtn/ToTopBtn.styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const ScrollBtn = styled.button`
   position: fixed;
-  bottom: 50px;
+  bottom: 30px;
   right: 20px;
   width: 50px;
   height: 50px;
