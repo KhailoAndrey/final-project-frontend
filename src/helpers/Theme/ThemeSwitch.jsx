bfe7f7586bebb8153c3c73d94bf091122af9db8a
Maryna K <110280285+Omenat83@users.noreply.github.com>
2023-08-04 02:29:08 +0300

    dark theme

---
+++ src/helpers/Theme/ThemeSwitch.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useTheme } from 'hooks/use-theme';
+import { DarkModeToggle } from 'react-dark-mode-toggle-2';
+import { SwitchContainer } from './ThemeSwitch.styled';
+
+export const ThemeSwitch = () => {
+  const [isDarkMode, setIsDarkMode] = useState(
+    localStorage.getItem('app-theme') === 'light' ? false : true
+  );
+
+  const { setTheme } = useTheme();
+
+  const toggleDarkMode = () => {
+    if (isDarkMode) {
+      setIsDarkMode(false);
+      setTheme('light');
+    } else {
+      setIsDarkMode(true);
+      setTheme('dark');
+    }
+  };
+
+  return (
+    <SwitchContainer>
+      <DarkModeToggle
+        onChange={toggleDarkMode}
+        checked={isDarkMode}
+        isDarkMode={isDarkMode}
+        size="60px"
+      />
+    </SwitchContainer>
+  );
+};
