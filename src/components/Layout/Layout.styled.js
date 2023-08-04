814d2a711b7944d987f413a812724094924f4c07
KhailoAndrey <hap512@gmail.com>
2023-08-04 11:43:21 +0300

    Update Layout.styled.js

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -16,7 +16,7 @@ export const Section = styled.section`
   width: 100%;
   height: 100%;
   /* min-width: 100vh;
-  min-height: 100vh; *
+  min-height: 100vh; */
   background-repeat: repeat;
   background-image: url(${bg_mob});
   /* overflow: auto; */
